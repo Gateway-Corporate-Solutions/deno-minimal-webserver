5c7720526ff77f771f1d2594496ffc21f9578aae
Sam <samroux808@gmail.com>
2025-07-21 13:29:14 -0500

    Added referral logic

--- static/index.js
+++ static/index.js
@@ -1,25 +1,11 @@
-const demoBtn = document.getElementById('demo-btn');
-const productsBtn = document.getElementById('products-btn');
-const menuBtn = document.getElementById('menu-btn');
-const dropdownMenu = document.getElementById('dropdown-menu');
-const closeMenu = document.getElementById('close-menu');
-
-demoBtn?.addEventListener('click', () => {
-    alert('Demo feature coming soon!');
-});
-
-productsBtn?.addEventListener('click', () => {
-    alert('Product details coming soon!');
-});
-
-menuBtn?.addEventListener('click', () => {
-    if (dropdownMenu) {
-        dropdownMenu.classList.add('show');
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+if (urlParams.has('referral')) {
+  const referral = urlParams.get('referral');
+  if (referral) {
+    const input = document.querySelector('input[name="referral"]');
+    if (input) {
+      input.value = referral;
     }
-});
-
-closeMenu?.addEventListener('click', () => {
-    if (dropdownMenu) {
-        dropdownMenu.classList.remove('show');
-    }
-});
+  }
+}
