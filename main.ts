3c73856fa913d1434c1574426aa35e870a303890
Sam <samroux808@gmail.com>
2025-07-21 09:39:36 -0500

    Added views directory for organization

--- main.ts
+++ main.ts
@@ -4,8 +4,17 @@ import { Application, Router} from "oak";
 const router = new Router();
 const app = new Application();
 router.get("/", (context) => {
-    context.response.body = Deno.readTextFileSync("./static/index.html");
-})
+    context.response.body = Deno.readTextFileSync("./static/views/index.html");
+});
+router.get("/:view.html", (context) => {
+    const view = context.params.view;
+    if (view) {
+        context.response.body = Deno.readTextFileSync(`./static/views/${view}.html`);
+    } else {
+        context.response.status = 404;
+        context.response.body = "View not found";
+    }
+});
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(async (context, next) => {
