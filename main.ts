67d52ca9770f141358109ffd47dd8527af089816
Sam <samroux808@gmail.com>
2025-07-22 14:04:39 -0500

    Stripped project back to minimal server

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { Application, Router} from "oak";
-import { handleUserRequest } from "./contact.ts";
 
 const router = new Router();
 const app = new Application();
@@ -22,33 +21,6 @@ router.get("/:view.html", (context) => {
         context.response.status = 404;
         context.response.body = "View not provided";
     }
-});
-router.get("/papers/:paper.pdf", (context) => {
-    const paper = context.params.paper;
-    if (paper) {
-        try {
-            context.response.body = Deno.readFileSync(`./static/papers/${paper}.pdf`);
-            context.response.headers.set("Content-Type", "application/pdf");
-        } catch (error) {
-            console.error(`Error reading paper file: ${error}`);
-            context.response.status = 404;
-            context.response.body = "Paper not found";
-        }
-    } else {
-        context.response.status = 404;
-        context.response.body = "Paper not provided";
-    }
-})
-router.post('/contact', async (context) => {
-  try {
-    const form = await context.request.body.form();
-    await handleUserRequest(form);
-    context.response.redirect('/');
-  } catch (error) {
-    console.error('Error processing request:', error);
-    context.response.status = 500;
-    context.response.body = 'Internal Server Error';
-  }
 });
 
 app.use(router.routes());
